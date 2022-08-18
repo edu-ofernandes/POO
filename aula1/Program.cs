21abbe59b9988b8df2926cb5e3a10c574487d223
Eduardo Oliveira <eduardo123fernandes2013@hotmail.com>
2022-08-17 21:00:41 -0300

    feat(aula1): first commit

---
+++ aula1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite o número do mês: ");
+int mes = int.Parse(Console.ReadLine());
+
+switch(mes) {
+    case 1:
+        Console.WriteLine("O mês escolhido é Janeiro");
+        break;
+    default:
+        Console.WriteLine("Informe um valor entre 1 e 12.");
+        break;
+}
