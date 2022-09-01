45dd8ef8f2ca9361942edbcaec5f6874a8972839
Eduardo Oliveira <eduardo123fernandes2013@hotmail.com>
2022-08-31 21:09:51 -0300

    wip productArray

---
+++ ArrayProduto/Produto.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArrayProduto
+{
+    public class Produto
+    {
+        public int codigo;
+        public string? descricao;
+        public double preco;
+
+        public void show() {
+            Console.WriteLine(this.codigo);
+            Console.WriteLine(this.descricao);
+            Console.WriteLine(this.preco);
+        }
+
+        public void addPercentage(double percent) {
+            this.preco = this.preco + (this.preco * percent);
+        }
+    }
+}
---
+++ ArrayProduto/Program.cs
@@ -0,0 +1,27 @@
+﻿using ArrayProduto;
+
+Produto[] produtos  = new Produto[5];
+
+for (int i = 0; i < produtos.Length; i++) {
+    produtos[i] = new Produto();
+    // Produto produto = produtos[i];
+
+    Console.WriteLine("Informe o descricao, preço e código!!");
+
+    Console.WriteLine("Informe a descrição");
+    produtos[i].descricao = Console.ReadLine();
+
+    Console.WriteLine("Informe o código");
+    produtos[i].codigo = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Informe o valor");
+    produtos[i].preco = Convert.ToDouble(Console.ReadLine());
+
+    // produtos[i].add(produtos[i]);    
+}
+
+Console.WriteLine("Informe o descricao, preço e código!!");
+
+foreach (Produto produto in produtos) {
+    produto.show();
+}
